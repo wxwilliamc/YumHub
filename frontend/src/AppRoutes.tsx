5a18502485e2591b13876342ceda6964496a5093
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2024-02-20 10:59:58 +0800

    fn - add protected route on user profile

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,13 +3,16 @@ import Layout from "./layouts/layout"
 import Homepage from "./pages/Homepage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout showHero><Homepage /></Layout>} />
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+            </Route>
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
